9704749c10a2e196d7c4c41148f962a9d03dd159
Eduardo Brites <eduardocoutinho30089@gmail.com>
2026-05-21 09:44:30 -0300

    adjust: Alterando a interpretação do cálculo na memória de cálculo

--- src/components/escalas/processors/tc10m.js
+++ src/components/escalas/processors/tc10m.js
@@ -10,7 +10,7 @@ export const processarTC10m = (respostas) => {
         "descricao": "Teste de Caminhada de 10 Metros (TC-10m)",
         "doi": "10.1590/S1413-35552011000200006",
         "nome_curto": "TC10m",
-        "calculo": "Escore = max(p2, p3, p4)",
+        "calculo": "Escore = max(p3, p4, p5) || max(10 / p6, 10 / p7, 10 / p8)",
         "calculo_processado": "Padronização Radar = ((base - 0.8) / (1.3 - 0.8)) * 10",
         "interpretacao": "O escore bruto é o maior valor dentre os três informados no formulário. Valor mínimo = 0.8; Valor máximo = 1.3; Valores fora do intervalo viram 0 (se menor que 0.8 m/s) ou 10 (se maior que 1.3 m/s). O escore padronizado lineariza a relação de tal que forma que uma resposta máxima (1.3 no escore bruto) seja um valor no radar igual a 10. O valor mínimo fica com 0 e qualquer outra pontuação está interpolada linearmente"
     };
